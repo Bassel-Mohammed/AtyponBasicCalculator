27f969642a18dff94bc0baf3618cd9f0a2498a9d
Bassel-Mohammed <bassel.alramoni@gmail.com>
2025-01-11 19:14:31 +0300

    Add files via upload
    created the structure of the project

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static int summation(int x , int y){
+        return x + y;
+    }
+
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 2;
+
+        System.out.println(summation(x , y));
+
+
+
+
+    }
+}
